ed1f6a49da47cf480f0ea3af8cd956ef4cbb8707
wolverine <wolverine@mbp-de-julio-001>
2017-09-04 11:48:31 -0300

    teste julio

---
+++ src/br/com/travelmate/managerBean/financeiro/crmcobranca/TesteJulio.java
@@ -0,0 +1,20 @@
+package br.com.travelmate.managerBean.financeiro.crmcobranca;
+
+import javax.inject.Named;
+
+@Named
+public class TesteJulio {
+	
+	private int numero=1;
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	
+
+}
