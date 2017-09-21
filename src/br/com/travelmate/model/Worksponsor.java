34f2f08cfddbe50af9ab73d689066bffa906e6f8
wolverine <wolverine@mbp-de-julio-001>
2017-09-21 14:13:53 -0300

    Model worksponsorarquivo

--- src/br/com/travelmate/model/Worksponsor.java
+++ src/br/com/travelmate/model/Worksponsor.java
@@ -1,7 +1,10 @@
 package br.com.travelmate.model;
 
 import java.io.Serializable;
+import java.util.List;
+
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
@@ -68,6 +72,8 @@ public class Worksponsor implements Serializable {
     @JoinColumn(name = "fornecedorcidade_idfornecedorcidade", referencedColumnName = "idfornecedorcidade")
     @ManyToOne(optional = false)
     private Fornecedorcidade fornecedorcidade;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "worksponsor")
+    private List<Worksponsorarquivos> worksponsorarquivosList;
 
     public Worksponsor() {
     }
---
+++ src/br/com/travelmate/model/Worksponsorarquivos.java
@@ -0,0 +1,114 @@
+package br.com.travelmate.model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author wolverine
+ */
+@Entity
+@Table(name = "worksponsorarquivos")
+public class Worksponsorarquivos implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idworksponsorarquivos")
+    private Integer idworksponsorarquivos;
+    @Size(max = 100)
+    @Column(name = "nome")
+    private String nome;
+    @Size(max = 100)
+    @Column(name = "nomeftp")
+    private String nomeftp;
+    @JoinColumn(name = "tipoarquivo_idtipoArquivo", referencedColumnName = "idtipoArquivo")
+    @ManyToOne(optional = false)
+    private Tipoarquivo tipoarquivo;
+    @JoinColumn(name = "worksponsor_idworksponsor", referencedColumnName = "idworksponsor")
+    @ManyToOne(optional = false)
+    private Worksponsor worksponsor;
+
+    public Worksponsorarquivos() {
+    }
+
+    public Worksponsorarquivos(Integer idworksponsorarquivos) {
+        this.idworksponsorarquivos = idworksponsorarquivos;
+    }
+
+    public Integer getIdworksponsorarquivos() {
+        return idworksponsorarquivos;
+    }
+
+    public void setIdworksponsorarquivos(Integer idworksponsorarquivos) {
+        this.idworksponsorarquivos = idworksponsorarquivos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeftp() {
+        return nomeftp;
+    }
+
+    public void setNomeftp(String nomeftp) {
+        this.nomeftp = nomeftp;
+    }
+
+    public Tipoarquivo getTipoarquivo() {
+        return tipoarquivo;
+    }
+
+    public void setTipoarquivo(Tipoarquivo tipoarquivo) {
+        this.tipoarquivo = tipoarquivo;
+    }
+
+    public Worksponsor getWorksponsor() {
+        return worksponsor;
+    }
+
+    public void setWorksponsor(Worksponsor worksponsor) {
+        this.worksponsor = worksponsor;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idworksponsorarquivos != null ? idworksponsorarquivos.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Worksponsorarquivos)) {
+            return false;
+        }
+        Worksponsorarquivos other = (Worksponsorarquivos) object;
+        if ((this.idworksponsorarquivos == null && other.idworksponsorarquivos != null) || (this.idworksponsorarquivos != null && !this.idworksponsorarquivos.equals(other.idworksponsorarquivos))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.travelmate.model.Worksponsorarquivos[ idworksponsorarquivos=" + idworksponsorarquivos + " ]";
+    }
+    
+}
