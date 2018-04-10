1f6db59cf5c29b6dee09964033c4c5c3ee92eda7
andersonluizmachadodearaujo <andersonluizmachadodearaujo@192.168.0.12>
2018-04-10 10:16:30 -0300

    correção cadastro de histórico

---
+++ src/br/com/travelmate/converter/TipoContatoConverterLead.java
@@ -0,0 +1,48 @@
+package br.com.travelmate.converter;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import br.com.travelmate.model.Tipocontato;
+
+@FacesConverter(value = "TipoContatoConverterLead")
+public class TipoContatoConverterLead implements Converter{
+	
+	
+	@Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        List<Tipocontato> listaTipocontato = (List<Tipocontato>) component.getAttributes().get("listaTipocontato");
+        if (listaTipocontato != null) {
+            for (Tipocontato tipocontato : listaTipocontato) {
+                if (tipocontato.getTipo().equalsIgnoreCase(value)) {
+                    return tipocontato;
+                }
+            }
+        } else {
+        	Tipocontato tipocontato = new Tipocontato();
+            return tipocontato;
+        }
+        Tipocontato tipocontato = new Tipocontato();
+        return tipocontato;
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        if (value.toString().equalsIgnoreCase("0")) {
+            return "Selecione";
+        } else {
+        	Tipocontato tipocontato = (Tipocontato) value;
+            if (tipocontato.getIdtipocontato() != null) {
+                return tipocontato.getTipo();
+            } else {
+                return "";
+            }
+        }
+    }
+
+}
