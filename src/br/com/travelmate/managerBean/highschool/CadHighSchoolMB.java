c216d070eeceb4cdd9fd8499bf1b8f478705d24b
andersonluizmachadodearaujo <andersonluizmachadodearaujo@mbp-de-anderson>
2018-04-13 12:58:36 -0300

    correção desconto TM

--- src/br/com/travelmate/managerBean/highschool/CadHighSchoolMB.java
+++ src/br/com/travelmate/managerBean/highschool/CadHighSchoolMB.java
@@ -968,24 +968,36 @@ public class CadHighSchoolMB implements Serializable {
 					if (produtosorcamento.getIdprodutosOrcamento() != idProduto) {
 						orcamentoprodutosorcamento.setDescricao(produtosorcamento.getDescricao());
 						orcamentoprodutosorcamento.setProdutosorcamento(produtosorcamento);
-						if ((orcamentoprodutosorcamento.getValorMoedaEstrangeira() > 0)
-								&& (orcamento.getValorCambio() > 0)) {
-							orcamentoprodutosorcamento.setValorMoedaNacional(
-									orcamentoprodutosorcamento.getValorMoedaEstrangeira() * orcamento.getValorCambio());
+						
+//						if ((orcamentoprodutosorcamento.getValorMoedaEstrangeira() > 0)
+//								&& (orcamento.getValorCambio() > 0)) {
+//							orcamentoprodutosorcamento.setValorMoedaNacional(
+//									orcamentoprodutosorcamento.getValorMoedaEstrangeira() * orcamento.getValorCambio());
+//						} else {
+//							if ((orcamentoprodutosorcamento.getValorMoedaNacional() > 0)
+//									&& (orcamento.getValorCambio() > 0)) {
+//								orcamentoprodutosorcamento
+//										.setValorMoedaEstrangeira(orcamentoprodutosorcamento.getValorMoedaNacional()
+//												/ orcamento.getValorCambio());
+//							}
+//						}
+						if ((valorMoedaEstrangeira > 0) && (orcamento.getValorCambio() > 0)) {
+							valorMoedaReal = valorMoedaEstrangeira * orcamento.getValorCambio();
 						} else {
-							if ((orcamentoprodutosorcamento.getValorMoedaNacional() > 0)
-									&& (orcamento.getValorCambio() > 0)) {
-								orcamentoprodutosorcamento
-										.setValorMoedaEstrangeira(orcamentoprodutosorcamento.getValorMoedaNacional()
-												/ orcamento.getValorCambio());
+							if ((valorMoedaReal > 0) && (orcamento.getValorCambio() > 0)) {
+								valorMoedaEstrangeira = valorMoedaReal / orcamento.getValorCambio();
 							}
 						}
 						boolean excluirDescontoTM = true;
 						if (produtosorcamento.getValormaximo()==0) {
+							orcamentoprodutosorcamento . setValorMoedaEstrangeira (valorMoedaEstrangeira);
+							orcamentoprodutosorcamento . setValorMoedaNacional (valorMoedaReal);
 							orcamento.getOrcamentoprodutosorcamentoList().add(orcamentoprodutosorcamento);
 							calcularValorTotalOrcamento();
 							orcamentoprodutosorcamento = new Orcamentoprodutosorcamento();
-						}else if (produtosorcamento.getValormaximo()>=orcamentoprodutosorcamento.getValorMoedaNacional()){
+						}else if (produtosorcamento.getValormaximo()>=valorMoedaReal){
+							orcamentoprodutosorcamento . setValorMoedaEstrangeira (valorMoedaEstrangeira);
+							orcamentoprodutosorcamento . setValorMoedaNacional (valorMoedaReal);
 							orcamento.getOrcamentoprodutosorcamentoList().add(orcamentoprodutosorcamento);
 							calcularValorTotalOrcamento();
 							orcamentoprodutosorcamento = new Orcamentoprodutosorcamento();
@@ -1040,16 +1052,11 @@ public class CadHighSchoolMB implements Serializable {
 				Orcamentoprodutosorcamento orcamentoprodutosorcamento = new Orcamentoprodutosorcamento();
 				orcamentoprodutosorcamento.setDescricao(filtroorcamentoproduto.getProdutosorcamento().getDescricao());
 				orcamentoprodutosorcamento.setProdutosorcamento(filtroorcamentoproduto.getProdutosorcamento());
-				if ((orcamentoprodutosorcamento.getValorMoedaEstrangeira() > 0)
-						&& (orcamento.getValorCambio() > 0)) {
-					orcamentoprodutosorcamento.setValorMoedaNacional(
-							orcamentoprodutosorcamento.getValorMoedaEstrangeira() * orcamento.getValorCambio());
+				if ((valorMoedaEstrangeira > 0) && (orcamento.getValorCambio() > 0)) {
+					valorMoedaReal = valorMoedaEstrangeira * orcamento.getValorCambio();
 				} else {
-					if ((orcamentoprodutosorcamento.getValorMoedaNacional() > 0)
-							&& (orcamento.getValorCambio() > 0)) {
-						orcamentoprodutosorcamento
-								.setValorMoedaEstrangeira(orcamentoprodutosorcamento.getValorMoedaNacional()
-										/ orcamento.getValorCambio());
+					if ((valorMoedaReal > 0) && (orcamento.getValorCambio() > 0)) {
+						valorMoedaEstrangeira = valorMoedaReal / orcamento.getValorCambio();
 					}
 				}
 				orcamentoprodutosorcamento . setValorMoedaEstrangeira (valorMoedaEstrangeira);
