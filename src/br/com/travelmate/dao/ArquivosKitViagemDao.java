0bc713003189ced761d008faac67e2ffefa9092f
andersonluizmachadodearaujo <andersonluizmachadodearaujo@mbp-de-anderson>
2018-04-23 15:47:45 -0300

    Arquivo kit Viagem Dao

--- src/br/com/travelmate/dao/ArquivosKitViagemDao.java
+++ src/br/com/travelmate/dao/ArquivosKitViagemDao.java
@@ -11,11 +11,12 @@ import br.com.travelmate.model.Arquivoskitviagem;
 public class ArquivosKitViagemDao {
 	
 	public Arquivoskitviagem salvar(Arquivoskitviagem kitViagem) throws SQLException{
-		EntityManager manager = ConectionFactory.getInstance(); 
+		EntityManager manager = ConectionFactory.getConnection(); 
 		EntityTransaction tx = manager.getTransaction();
 		tx.begin();
 		kitViagem = manager.merge(kitViagem); 
 		tx.commit();
+		manager.close();
 		return kitViagem;
 	}
 
