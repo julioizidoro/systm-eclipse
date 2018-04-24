be17459cb3e768a79f6c8d0575ec0c424662c956
julioizidoro <julioizidoro@192.168.0.3>
2018-04-24 14:56:11 -0300

    Correção calculo estorno no cancelamento

--- src/br/com/travelmate/managerBean/cancelamento/CalcularMultaCancelamentoBean.java
+++ src/br/com/travelmate/managerBean/cancelamento/CalcularMultaCancelamentoBean.java
@@ -3,16 +3,14 @@ package br.com.travelmate.managerBean.cancelamento;
 import java.util.List;
 
 import br.com.travelmate.facade.ContasReceberFacade;
-import br.com.travelmate.facade.HighSchoolFacade;
 import br.com.travelmate.facade.InvoiceFacade;
 import br.com.travelmate.managerBean.financeiro.contasReceber.EventoContasReceberBean;
 import br.com.travelmate.managerBean.financeiro.crmcobranca.CrmCobrancaBean;
 import br.com.travelmate.model.Cancelamento;
 import br.com.travelmate.model.Contasreceber;
-import br.com.travelmate.model.Faturafranquias;
-import br.com.travelmate.model.Highschool;
 import br.com.travelmate.model.Invoice;
 import br.com.travelmate.model.Usuario;
+import br.com.travelmate.model.Vendascomissao;
 
 
 public class CalcularMultaCancelamentoBean {
@@ -22,7 +20,7 @@ public class CalcularMultaCancelamentoBean {
 		int idcondicao = cancelamento.getCondicaocancelamento().getIdcondicaocancelamento();
 		if (idcondicao==1){
 			return 0.0f;
-		}else if ((idcondicao==2) || (idcondicao==3) || (idcondicao==10) || (idcondicao==17)){
+		}else if ((idcondicao==2) || (idcondicao==3) || (idcondicao==10) || (idcondicao==11)){
 			return (float) (cancelamento.getVendas().getValor() * 0.30);
 		}else if (idcondicao==4){ 
 			return  1500 * cancelamento.getVendas().getValorcambio();
@@ -38,24 +36,23 @@ public class CalcularMultaCancelamentoBean {
 			return 750 * cancelamento.getVendas().getValorcambio();
 		}else if (idcondicao==9){
 			return 850 * cancelamento.getVendas().getValorcambio();
-		}else if(idcondicao==12){
-			float valorMulta = (float) (cancelamento.getVendas().getValor() * 0.20);
-			valorMulta = valorMulta + cancelamento.getVendas().getVendascomissao().getTaxatm();
-			return valorMulta; 
-		}else if (idcondicao==13){
+		}else if(idcondicao==12) {
 			return 0.0f;
+		}else if (idcondicao==13){
+			return (float) (cancelamento.getVendas().getValor() * 0.60);
 		}else if (idcondicao==14){
-			return (float) ((cancelamento.getVendas().getValor()*0.60) + cancelamento.getVendas().getVendascomissao().getTaxatm());
+			return (float) (cancelamento.getVendas().getValor() * 0.10);
 		}else if (idcondicao==15){
-			return (float) ((cancelamento.getVendas().getValor()*0.10) + cancelamento.getVendas().getVendascomissao().getTaxatm());
-		}else if (idcondicao==16){
 			return 700 * cancelamento.getVendas().getValorcambio();
+		}else if (idcondicao==16){
+			return (float) (cancelamento.getVendas().getValor() * 0.30);
+		}else if (idcondicao==17){
+			return 0.0f;
 		}else if (idcondicao==18){
 			return 0.0f;
 		}else if ((idcondicao==19) || (idcondicao==20)){
 			return cancelamento.getVendas().getValor();
-		}
-		return 0.0f;
+		}else return 0.0f;
 	}
 	
 	public Cancelamento calcularTotais(Cancelamento cancelamento){
@@ -75,38 +72,36 @@ public class CalcularMultaCancelamentoBean {
 		int idcondicao = cancelamento.getCondicaocancelamento().getIdcondicaocancelamento();
 		if (idcondicao==1){
 			return 0.0f;
-		}else if ((idcondicao==2) || (idcondicao==3) || (idcondicao==12) || (idcondicao==13) || (idcondicao==14)
-				 || (idcondicao==15) || (idcondicao==16)){
+		}else if ((idcondicao==2) || (idcondicao==3) ||  (idcondicao==13) 
+				  || (idcondicao==16)){
 			return 0.0f;
 		}else if ((idcondicao==4) || (idcondicao==5)) {
-			HighSchoolFacade highSchoolFacade = new HighSchoolFacade();
-			Highschool highschool = highSchoolFacade.consultarHighschool(cancelamento.getVendas().getIdvendas());
-			if (highschool!=null){
-				if (highschool.getValoreshighschool().getDuracao().equalsIgnoreCase("02 SEMESTRES")){
-					return 725 * cancelamento.getVendas().getValorcambio();
-				}else return 375 * cancelamento.getVendas().getValorcambio();
-			}
-			return 375 * cancelamento.getVendas().getValorcambio();
+			return (float) (calcularComissaoFranquia(cancelamento.getVendas().getVendascomissao()) * 0.75);
 		}else if ((idcondicao==6) || (idcondicao==10)){
-			return 300 * cancelamento.getVendas().getValorcambio();
-		}else if ((idcondicao==7) || (idcondicao==8) || (idcondicao==9)){
-			return 250 * cancelamento.getVendas().getValorcambio();
-		}else if (idcondicao==8){
-			return 750 * cancelamento.getVendas().getValorcambio();
-		}else if (idcondicao==9){
-			return 850 * cancelamento.getVendas().getValorcambio();
-		}else if(idcondicao==11){
-			return 672 * cancelamento.getVendas().getValorcambio();
+			return  (float) (calcularComissaoFranquia(cancelamento.getVendas().getVendascomissao()) * 0.75);
+		}else if ((idcondicao==7) || (idcondicao==8) || (idcondicao==9) || (idcondicao==15)){
+			return  (float) (calcularComissaoFranquia(cancelamento.getVendas().getVendascomissao())* 0.75);
+		}else if((idcondicao==11) ){
+			return 0.0f;
 		}else if (idcondicao==17){
-			return 150 * cancelamento.getVendas().getValorcambio();
+			return calcularComissaoFranquia(cancelamento.getVendas().getVendascomissao());
 		}else if (idcondicao==18){
-			return cancelamento.getVendas().getVendascomissao().getLiquidofranquia();
+			return 0.0f;
 		}else if (idcondicao==19){
 			return 0.0f;
 		}else if (idcondicao==20){
 			return 0.0f;
-		}
-		return 0.0f;
+		}else if (idcondicao==29) {
+			return 0.0f;
+		}else if ((idcondicao==12) || (idcondicao==14)) {
+			return calcularComissaoFranquia(cancelamento.getVendas().getVendascomissao());
+		
+		}return 0.0f;
+	}
+	
+	public float calcularComissaoFranquia(Vendascomissao vendaComissao) {
+		float valorComissao = vendaComissao.getLiquidofranquia() + (vendaComissao.getCustofinanceirofranquia()/2);
+		return valorComissao;
 	}
 	
 	public boolean verifcarValorCreditoReembolso(Cancelamento cancelamento){
