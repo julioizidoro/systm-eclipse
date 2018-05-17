67efe213ae8a045193989b059dee8ed8601cc4d6
andersonluizmachadodearaujo <andersonluizmachadodearaujo@192.168.100.50>
2018-05-17 11:24:21 -0300

    correções escrita  (Emissão High School)

--- src/br/com/travelmate/managerBean/highschool/CadHighSchoolMB.java
+++ src/br/com/travelmate/managerBean/highschool/CadHighSchoolMB.java
@@ -579,7 +579,7 @@ public class CadHighSchoolMB implements Serializable {
 		if (cidade != null) {
 			String sql = "select f from Fornecedorcidade f where f.produtos.idprodutos="
 					+ aplicacaoMB.getParametrosprodutos().getHighSchool() + " and f.cidade.idcidade="
-					+ cidade.getIdcidade() + " and f.ativo=1";
+					+ cidade.getIdcidade() + " and f.ativo=1 order by f.fornecedor.nome ";
 			FornecedorCidadeFacade fornecedorCidadeFacade = new FornecedorCidadeFacade();
 			listaFornecedorCidade = fornecedorCidadeFacade.listar(sql);
 			if (listaFornecedorCidade == null) {
