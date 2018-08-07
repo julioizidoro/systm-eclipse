0c5d62aadbd8ec8e29919849f6333a7fa726ce4b
Anderson <Anderson@Andrieli-PC>
2018-08-07 17:35:00 -0300

    Arquivos Lista Modelo Dao

---
+++ src/br/com/travelmate/dao/ArquivosListaModeloDao.java
@@ -0,0 +1,29 @@
+package br.com.travelmate.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.travelmate.model.Arquivoslistamodelo;
+import br.com.travelmate.model.Vendas;
+
+public class ArquivosListaModeloDao implements Serializable{
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Inject
+	private EntityManager manager;
+	
+	public List<Arquivoslistamodelo> lista(String sql) {
+        Query q = manager.createQuery(sql);
+        List<Arquivoslistamodelo> lista = q.getResultList();
+        return lista;
+    }
+
+}
